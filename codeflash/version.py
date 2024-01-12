d79059d2b35934cb2975e905e55fddf874f4e569
afik.cohen <aphexsa@gmail.com>
2024-01-12 13:58:48 -0800

    Del unused run tmp file from aiservice

--- codeflash/version.py
+++ codeflash/version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced by poetry-dynamic-versioning during `poetry build`.
-__version__ = "0.4.3.post127.dev0+214a2bd"
-__version_tuple__ = (0, 4, 3, "post127", "dev0", "214a2bd")
+__version__ = "0.1.0"
+__version_tuple__ = (0, 1, 0)
