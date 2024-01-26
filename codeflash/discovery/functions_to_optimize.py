68aed1ec52d08adfdc56cf798620b441105f560e
Saurabh Misra <misra.saurabh1@gmail.com>
2024-01-25 19:29:06 -0800

    add logs

--- codeflash/discovery/functions_to_optimize.py
+++ codeflash/discovery/functions_to_optimize.py
@@ -140,9 +140,12 @@ def get_functions_within_git_diff() -> Dict[str, List[FunctionToOptimize]]:
     print("MODIFIED LINES", modified_lines)
     modified_functions: Dict[str, List[FunctionToOptimize]] = {}
     for path in modified_lines:
+        print("PATH", path)
         if not os.path.exists(path):
+            print("PATH DOES NOT EXIST", path)
             continue
         with open(path, "r") as f:
+            print("OPENING FILE", path)
             file_content = f.read()
             wrapper = cst.metadata.MetadataWrapper(cst.parse_module(file_content))
             function_lines = FunctionVisitor(file_path=path)
@@ -154,6 +157,7 @@ def get_functions_within_git_diff() -> Dict[str, List[FunctionToOptimize]]:
                     if path not in modified_functions:
                         modified_functions[path] = []
                     modified_functions[path].append(function_to_optimize)
+    print("MODIFIED FUNCTIONS", modified_functions)
     return modified_functions
 
 
