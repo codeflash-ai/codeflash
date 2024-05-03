e6a137536c070293e990fe1deb6237d34d81f4d2
afik.cohen <aphexsa@gmail.com>
2024-05-02 17:51:33 -0700

    Add wip performance metrics tests

--- experiments/tests/test_metrics_analysis.py
+++ experiments/tests/test_metrics_analysis.py
@@ -93,5 +93,58 @@ def test_calculate_validity_with_valid_candidates(pie4perf_sample_dataframe):
     assert validity_metrics["percent_valid_candidates"] == expected_percent_valid_candidates
 
 
+# def test_calculate_performance_no_successful_runs(pie4perf_sample_dataframe):
+#     df = pie4perf_sample_dataframe
+#     df["original_runtime"] = [nan, nan, nan, nan, nan, nan]  # No successful runs
+#     df["best_correct_speedup_ratio"] = [nan, nan, nan, nan, nan, nan]  # No valid candidates
+#     performance_metrics = calculate_performance(df)
+#     assert performance_metrics["average_percentage_gain_pr"] == 0
+#     assert performance_metrics["geometric_mean_gain_pr"] == 0
+#     assert performance_metrics["mean_average_percentage_gain_all"] == 0
+#     assert performance_metrics["geometric_mean_gain_all"] == 0
+#     assert performance_metrics["average_time_saved_pr"] == 0
+#     assert performance_metrics["mean_average_time_saved_all"] == 0
+#
+#
+# def test_calculate_performance_some_successful_runs(pie4perf_sample_dataframe):
+#     df = pie4perf_sample_dataframe
+#     # Assuming half the runs are successful and above threshold, the expected value should be 50.0
+#     performance_metrics = calculate_performance(df)
+#     assert performance_metrics["average_percentage_gain_pr"] == pytest.approx(0.02112, 0.001)
+#     assert performance_metrics["geometric_mean_gain_pr"] == pytest.approx(1.02112, 0.001)
+#     assert performance_metrics["mean_average_percentage_gain_all"] == pytest.approx(0.02112, 0.001)
+#     assert performance_metrics["geometric_mean_gain_all"] == pytest.approx(1.02112, 0.001)
+#     assert performance_metrics["average_time_saved_pr"] == pytest.approx(0.0, 0.001)
+#     assert performance_metrics["mean_average_time_saved_all"] == pytest.approx(0.0, 0.001)
+#
+#
+# def test_calculate_performance_all_successful_runs(pie4perf_sample_dataframe):
+#     df = pie4perf_sample_dataframe
+#     df["original_runtime"] = [1.0, 2.0, 2.0, 2.0, 3.0, 4.0]  # All successful runs
+#     df["best_correct_speedup_ratio"] = [
+#         0.05,
+#         0.06,
+#         0.08,
+#         0.09,
+#         0.56,
+#         0.57,
+#     ]  # All above threshold
+#     df["is_correct"] = [
+#         {"1f39ef86-5eff-4760-a262-43011492906e": True},
+#         {"aeeeca3b-4ccf-46eb-8dbf-c526c05fca27": True},
+#         {"dc67527a-9cfe-4bbe-98eb-266bc8e9a27d": True},
+#         {"e584d267-f68f-4144-820f-110c2719919f": True},
+#         {"e584d267-f68f-4144-820f-110c2719919f": True},
+#         {"d34db33f-f68f-4144-820f-110c2719919f": True},
+#     ]
+#     performance_metrics = calculate_performance(df)
+#     assert performance_metrics["average_percentage_gain_pr"] == pytest.approx(0.235, 0.001)
+#     assert performance_metrics["geometric_mean_gain_pr"] == pytest.approx(1.214, 0.001)
+#     assert performance_metrics["mean_average_percentage_gain_all"] == pytest.approx(0.235, 0.001)
+#     assert performance_metrics["geometric_mean_gain_all"] == pytest.approx(1.214, 0.001)
+#     assert performance_metrics["average_time_saved_pr"] == pytest.approx(0.0, 0.001)
+#     assert performance_metrics["mean_average_time_saved_all"] == pytest.approx(0.0, 0.001)
+#
+
 if __name__ == "__main__":
     pytest.main()
