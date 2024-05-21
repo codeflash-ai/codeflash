365cb811fc587f0df37cd642d1c9cdaed7694443
Saurabh Misra <misra.saurabh1@gmail.com>
2024-05-20 20:21:24 -0700

    Revert "find common tags"
    This reverts commit b5a26d2d53a17e3cc949d4b3a350f36a19c7a7fc.

--- codeflash/result/common_tags.py
+++
@@ -1,8 +0,0 @@
-def find_common_tags(articles: list[dict[str, list[str]]]) -> set[str]:
-    if not articles:
-        return set()
-
-    common_tags = articles[0]["tags"]
-    for article in articles[1:]:
-        common_tags = [tag for tag in common_tags if tag in article["tags"]]
-    return set(common_tags)
