0c84f2e5b3bf1ebb9d9b8e52a6d609305dca45d3
RD <92499101+iusedmyimagination@users.noreply.github.com>
2024-10-04 18:02:54 -0700

    Continuing fixes.

--- code_to_optimize/bubble_sort.py
+++ code_to_optimize/bubble_sort.py
@@ -1,13 +1,10 @@
 def sorter(arr):
-    n = len(arr)
-    for i in range(n):
-        swapped = False
-        for j in range(n - i - 1):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if not swapped:
-            break
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
     return arr
 
 
