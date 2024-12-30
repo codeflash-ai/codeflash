530e9d9f663607b0a8e3769d507ba548cb89ed96
Saurabh Misra <misra.saurabh1@gmail.com>
2024-12-30 12:23:15 -0800

    Fix a typing issue

--- codeflash/api/aiservice.py
+++ codeflash/api/aiservice.py
@@ -172,7 +172,7 @@ class AiServiceClient:
         test_timeout: int,
         trace_id: str,
         test_index: int,
-    ) -> tuple[str, str] | None:
+    ) -> tuple[str, str, str] | None:
         """Generate regression tests for the given function by making a request to the Django endpoint.
 
         Parameters
