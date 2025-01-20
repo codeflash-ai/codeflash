75d33c3eedec7dfe1317c33d80722a648a940fe3
Kevin Turcios <turcioskevinr@gmail.com>
2025-01-20 18:51:52 -0500

    revert changes

--- codeflash/discovery/pytest_new_process_discovery.py
+++ codeflash/discovery/pytest_new_process_discovery.py
@@ -1,36 +1,20 @@
-from __future__ import annotations
-
-import logging
-import pickle
 import sys
-from pathlib import Path
 from typing import Any
 
-import pytest
-
-logger = logging.getLogger(__name__)
 # This script should not have any relation to the codeflash package, be careful with imports
 cwd = sys.argv[1]
 tests_root = sys.argv[2]
 pickle_path = sys.argv[3]
 collected_tests = []
 pytest_rootdir = None
-sys.path.insert(1, cwd)
+sys.path.insert(1, str(cwd))
 
 
 class PytestCollectionPlugin:
-    def __init__(self) -> None:
-        self.pytest_rootdir: Path | None = None
-
-    def pytest_collection_finish(self, session: pytest.Session) -> None:
+    def pytest_collection_finish(self, session) -> None:
+        global pytest_rootdir
         collected_tests.extend(session.items)
-        self.pytest_rootdir = session.config.rootdir  # type: ignore
-
-    def pytest_load_initial_conftests(self, early_config: pytest.Config) -> None:
-        """Disable any plugins that may be present during test collection."""
-        plugin_names = {dist.project_name for _, dist in early_config.pluginmanager.list_plugin_distinfo()}
-        for plugin_name in plugin_names:
-            early_config.pluginmanager.set_blocked(plugin_name)
+        pytest_rootdir = session.config.rootdir
 
 
 def parse_pytest_collection_results(pytest_tests: list[Any]) -> list[dict[str, str]]:
@@ -44,13 +28,17 @@ def parse_pytest_collection_results(pytest_tests: list[Any]) -> list[dict[str, s
 
 
 if __name__ == "__main__":
-    plugin = PytestCollectionPlugin()
+    import pytest
+
     try:
-        exitcode = pytest.main([tests_root, "-pno:logging", "--collect-only", "-m", "not skip"], plugins=[plugin])
+        exitcode = pytest.main(
+            [tests_root, "-pno:logging", "--collect-only", "-m", "not skip"], plugins=[PytestCollectionPlugin()]
+        )
     except Exception as e:
-        logger.exception("Failed to collect tests", exc_info=e)
+        print(f"Failed to collect tests: {e!s}")
         exitcode = -1
     tests = parse_pytest_collection_results(collected_tests)
+    import pickle
 
-    with Path(pickle_path).open("wb") as f:
-        pickle.dump((exitcode, tests, plugin.pytest_rootdir), f, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(pickle_path, "wb") as f:
+        pickle.dump((exitcode, tests, pytest_rootdir), f, protocol=pickle.HIGHEST_PROTOCOL)
