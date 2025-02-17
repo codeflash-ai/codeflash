c3e1c06d0f747b14073bdfe011dfaab07a2b69cc
Kevin Turcios <turcioskevinr@gmail.com>
2025-02-17 05:25:12 -0500

    add coverage expectation due to coverage bugfix

--- tests/scripts/end_to_end_test_init_optimization.py
+++ tests/scripts/end_to_end_test_init_optimization.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-from end_to_end_test_utilities import TestConfig, run_codeflash_command, run_with_retries
+from end_to_end_test_utilities import CoverageExpectation, TestConfig, run_codeflash_command, run_with_retries
 
 
 def run_test(expected_improvement_pct: int) -> bool:
@@ -10,6 +10,11 @@ def run_test(expected_improvement_pct: int) -> bool:
         function_name="CharacterRemover.remove_control_characters",
         test_framework="pytest",
         min_improvement_x=1.0,
+        coverage_expectations=[
+            CoverageExpectation(
+                function_name="CharacterRemover.remove_control_characters", expected_coverage=100.0, expected_lines=[14]
+            )
+        ],
     )
     cwd = (pathlib.Path(__file__).parent.parent.parent / "code_to_optimize").resolve()
     return run_codeflash_command(cwd, config, expected_improvement_pct)
