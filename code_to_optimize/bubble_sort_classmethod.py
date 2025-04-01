00abefbe6feec487b1079d353039f9875a3e4e68
Aseem Saxena <aseem.bits@gmail.com>
2025-03-31 18:36:32 -0700

    more tests

--- code_to_optimize/bubble_sort_classmethod.py
+++ code_to_optimize/bubble_sort_classmethod.py
@@ -1,6 +1,6 @@
-from code_to_optimize.bubble_sort_in_nested_class import WrapperClass
+from code_to_optimize.bubble_sort_in_class import BubbleSortClass
 
 
 def sort_classmethod(x):
-    y = WrapperClass.BubbleSortClass()
+    y = BubbleSortClass()
     return y.sorter(x)
