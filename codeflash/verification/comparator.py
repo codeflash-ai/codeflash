683c9f64a13153f894900629b0e97b366b019ee6
Alvin Ryanputra <alvin.ryanputra@gmail.com>
2025-04-17 16:01:33 -0400

    shift check for pickle placerholder access error in comparator

--- codeflash/verification/comparator.py
+++ codeflash/verification/comparator.py
@@ -65,11 +65,7 @@ def comparator(orig: Any, new: Any, superset_obj=False) -> bool:
             if len(orig) != len(new):
                 return False
             return all(comparator(elem1, elem2, superset_obj) for elem1, elem2 in zip(orig, new))
-        if isinstance(orig, PicklePlaceholderAccessError) or isinstance(new, PicklePlaceholderAccessError):
-            # If this error was raised, there was an attempt to access the PicklePlaceholder, which represents an unpickleable object.
-            # The test results should be rejected as the behavior of the unpickleable object is unknown.
-            logger.debug("Unable to verify behavior of unpickleable object in replay test")
-            return False
+
         if isinstance(
             orig,
             (
@@ -95,6 +91,11 @@ def comparator(orig: Any, new: Any, superset_obj=False) -> bool:
                 return True
             return math.isclose(orig, new)
         if isinstance(orig, BaseException):
+            if isinstance(orig, PicklePlaceholderAccessError) or isinstance(new, PicklePlaceholderAccessError):
+                # If this error was raised, there was an attempt to access the PicklePlaceholder, which represents an unpickleable object.
+                # The test results should be rejected as the behavior of the unpickleable object is unknown.
+                logger.debug("Unable to verify behavior of unpickleable object in replay test")
+                return False
             # if str(orig) != str(new):
             #     return False
             # compare the attributes of the two exception objects to determine if they are equivalent.
