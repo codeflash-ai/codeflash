5dfd15fdda528cee147b2932e782bc8691fb754c
Aseem Saxena <aseem.bits@gmail.com>
2025-05-19 22:23:29 -0400

    return length directly

--- codeflash/code_utils/code_utils.py
+++ codeflash/code_utils/code_utils.py
@@ -10,8 +10,8 @@ from tempfile import TemporaryDirectory
 
 from codeflash.cli_cmds.console import logger
 
-def encode_str(s: str) -> str:
-    return s[:len(s)//2]
+def encode_str(s: str) -> int:
+    return len(s)//2
 
 def get_qualified_name(module_name: str, full_qualified_name: str) -> str:
     if not full_qualified_name:
