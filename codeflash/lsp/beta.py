9c1d031047278c722447d28487708cdf0e82372b
Kevin Turcios <turcioskevinr@gmail.com>
2025-06-08 22:14:43 -0700

    restore getOptimizableFunctions

--- codeflash/lsp/beta.py
+++ codeflash/lsp/beta.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from pathlib import Path
 from typing import TYPE_CHECKING
+
+from pygls import uris
 
 from codeflash.lsp.server import CodeflashLanguageServer, CodeflashLanguageServerProtocol
 
@@ -25,10 +28,16 @@ server = CodeflashLanguageServer("codeflash-language-server", "v1.0", protocol_c
 
 @server.feature("getOptimizableFunctions")
 def get_optimizable_functions(
-    server: CodeflashLanguageServer,  # noqa: ARG001
-    params: OptimizableFunctionsParams,
+    server: CodeflashLanguageServer, params: OptimizableFunctionsParams
 ) -> dict[str, list[str]]:
-    return {params.textDocument.uri: ["example"]}
+    file_path = Path(uris.to_fs_path(params.textDocument.uri))
+    server.optimizer.args.file = file_path
+    server.optimizer.args.previous_checkpoint_functions = False
+    optimizable_funcs, _ = server.optimizer.get_optimizable_functions()
+    path_to_qualified_names = {}
+    for path, functions in optimizable_funcs.items():
+        path_to_qualified_names[path.as_posix()] = [func.qualified_name for func in functions]
+    return path_to_qualified_names
 
 
 if __name__ == "__main__":
