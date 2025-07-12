1fb06ec2a300cbe18b19518a9fdeb7a50efe60fc
mohammed <mohammed18200118@gmail.com>
2025-07-12 18:07:43 +0300

    clean up changes

--- codeflash/cli_cmds/cli.py
+++ codeflash/cli_cmds/cli.py
@@ -155,7 +155,6 @@ def process_pyproject_config(args: Namespace) -> Namespace:
         "disable_telemetry",
         "disable_imports_sorting",
         "git_remote",
-        "exp_git_remote",
         "override_fixtures",
     ]
     for key in supported_keys:
