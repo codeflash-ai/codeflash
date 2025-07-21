587c80cfa5c63d522e7ee4e7414ef1aab0edd0c5
Aseem Saxena <aseem.bits@gmail.com>
2025-07-21 22:34:08 +0000

    todo modify tests, modify js side

--- codeflash/api/cfapi.py
+++ codeflash/api/cfapi.py
@@ -119,6 +119,8 @@ def suggest_changes(
     generated_tests: str,
     trace_id: str,
     coverage_message: str,
+    replay_tests: str,
+    concolic_tests: str,
 ) -> Response:
     """Suggest changes to a pull request.
 
@@ -142,6 +144,8 @@ def suggest_changes(
         "generatedTests": generated_tests,
         "traceId": trace_id,
         "coverage_message": coverage_message,
+        "replay_tests":replay_tests,
+        "concolic_tests":concolic_tests
     }
     return make_cfapi_request(endpoint="/suggest-pr-changes", method="POST", payload=payload)
 
