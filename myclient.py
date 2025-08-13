f41af3d650f7c964d28c48dd314fbd1376009f92
aseembits93 <aseem.bits@gmail.com>
2025-08-12 17:53:36 -0700

    precommit mypy fix

--- myclient.py
+++ myclient.py
@@ -1,20 +1,18 @@
 import anthropic
-from rich import print
-url = 'https://0de03d07f8b9.ngrok-free.app'
+from rich import print as rprint
+
+url = "https://0de03d07f8b9.ngrok-free.app"
 client = anthropic.Anthropic()
 response = client.beta.messages.create(
     model="claude-sonnet-4-20250514",
     max_tokens=1000,
-    messages=[{"role": "user", "content": "Optimize my codebase, the file is \"/Users/codeflash/Downloads/codeflash-dev/codeflash/code_to_optimize/bubble_sort.py\" and the function is \"sorter\""}],
-    mcp_servers=[
+    messages=[
         {
-            "type": "url",
-            "url": f"{url}/mcp/",
-            "name": "Code Optimization Assistant",
+            "role": "user",
+            "content": 'Optimize my codebase, the file is "/Users/codeflash/Downloads/codeflash-dev/codeflash/code_to_optimize/bubble_sort.py" and the function is "sorter"',
         }
     ],
-    extra_headers={
-        "anthropic-beta": "mcp-client-2025-04-04"
-    }
+    mcp_servers=[{"type": "url", "url": f"{url}/mcp/", "name": "Code Optimization Assistant"}],
+    extra_headers={"anthropic-beta": "mcp-client-2025-04-04"},
 )
-print(response.content)
+rprint(response.content)
--- myserver.py
+++ myserver.py
@@ -1,11 +1,17 @@
 import pathlib
+
 from fastmcp import FastMCP
+
 from tests.scripts.end_to_end_test_utilities import CoverageExpectation, TestConfig, run_codeflash_command
-mcp = FastMCP(name="Code Optimization Assistant",
+
+mcp = FastMCP(
+    name="Code Optimization Assistant",
     instructions="""
         This server provides code optimization tools.
         Call optimize_code(file, function) to optimize your code.
-    """)
+    """,
+)
+
 
 @mcp.tool
 def optimize_code(file: str, function: str) -> bool:
@@ -20,10 +26,11 @@ def optimize_code(file: str, function: str) -> bool:
             )
         ],
     )
-    cwd = (pathlib.Path(__file__).parent/ "code_to_optimize").resolve() # TODO remove it
+    cwd = (pathlib.Path(__file__).parent / "code_to_optimize").resolve()  # TODO remove it
     return run_codeflash_command(
         cwd, config, 100, ['print("codeflash stdout: Sorting list")', 'print(f"result: {arr}")']
     )
+
 
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
