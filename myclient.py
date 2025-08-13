36c83b333ad836555e0c63125ae8805958a3e502
aseembits93 <aseem.bits@gmail.com>
2025-08-12 17:39:57 -0700

    works without hardcoded, need to remove cwd line

--- myclient.py
+++ myclient.py
@@ -1,11 +1,66 @@
-import asyncio
-from fastmcp import Client
+# import asyncio
+# from fastmcp import Client
+#
+# client = Client("myserver.py")
+#
+# async def call_tool(file: str, function: str)-> None:
+#     async with client:
+#         result = await client.call_tool("optimize_code", {"file": f"{file}", "function":f"{function}"})
+#         print(result)
+#
+# asyncio.run(call_tool("Ford","Mustang"))
 
-client = Client("myserver.py")
 
-async def call_tool(file: str, function: str)-> None:
-    async with client:
-        result = await client.call_tool("optimize_code", {"file": f"{file}", "function":f"{function}"})
-        print(result)
+import anthropic
+from rich import print
 
-asyncio.run(call_tool("Ford","Mustang"))
+# Your server URL (replace with your actual URL)
+url = 'https://0de03d07f8b9.ngrok-free.app'
+
+client = anthropic.Anthropic()
+
+#file and fn names are hard coded for now
+response = client.beta.messages.create(
+    model="claude-sonnet-4-20250514",
+    max_tokens=1000,
+    messages=[{"role": "user", "content": "Optimize my codebase, the file is \"/Users/codeflash/Downloads/codeflash-dev/codeflash/code_to_optimize/bubble_sort.py\" and the function is \"sorter\""}],
+    mcp_servers=[
+        {
+            "type": "url",
+            "url": f"{url}/mcp/",
+            "name": "HelpfulAssistant",
+        }
+    ],
+    extra_headers={
+        "anthropic-beta": "mcp-client-2025-04-04"
+    }
+)
+
+print(response.content)
+
+
+# import anthropic
+# from rich import print
+#
+# # Your server URL (replace with your actual URL)
+# url = 'https://0de03d07f8b9.ngrok-free.app'
+#
+# client = anthropic.Anthropic()
+#
+# response = client.beta.messages.create(
+#     model="claude-sonnet-4-20250514",
+#     max_tokens=1000,
+#     messages=[{"role": "user", "content": "Roll a few dice!"}],
+#     mcp_servers=[
+#         {
+#             "type": "url",
+#             "url": f"{url}/mcp/",
+#             "name": "Dice Roller",
+#         }
+#     ],
+#     extra_headers={
+#         "anthropic-beta": "mcp-client-2025-04-04"
+#     }
+# )
+#
+# print(response.content)
--- myserver.py
+++ myserver.py
@@ -1,29 +1,32 @@
-import os
 import pathlib
 
 from fastmcp import FastMCP
 
 from tests.scripts.end_to_end_test_utilities import CoverageExpectation, TestConfig, run_codeflash_command
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP(name="HelpfulAssistant",
+    instructions="""
+        This server provides code optimization tools.
+        Call optimize_code(file, function) to optimize your code.
+    """,)
 
 @mcp.tool
-def optimize_code(file: str, function: str) -> bool: #todo add file and function name as arguments
+def optimize_code(file: str, function: str) -> bool:
     config = TestConfig(
-        file_path=pathlib.Path("/Users/codeflash/Downloads/codeflash-dev/codeflash/code_to_optimize/bubble_sort.py"),
-        function_name="sorter",
+        file_path=pathlib.Path(file),
+        function_name=function,
         test_framework="pytest",
         min_improvement_x=1.0,
         coverage_expectations=[
             CoverageExpectation(
-                function_name="sorter", expected_coverage=100.0, expected_lines=[2, 3, 4, 5, 6, 7, 8, 9, 10]
+                function_name=function, expected_coverage=100.0, expected_lines=[2, 3, 4, 5, 6, 7, 8, 9, 10]
             )
         ],
     )
-    cwd = (pathlib.Path(__file__).parent/ "code_to_optimize").resolve()
+    cwd = (pathlib.Path(__file__).parent/ "code_to_optimize").resolve() # todo remove it
     return run_codeflash_command(
         cwd, config, 100, ['print("codeflash stdout: Sorting list")', 'print(f"result: {arr}")']
     )
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(transport="http", port=8000)
