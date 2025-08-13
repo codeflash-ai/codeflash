f65bfc893f8fd3c65eb565a1104887201169503e
aseembits93 <aseem.bits@gmail.com>
2025-08-12 17:42:52 -0700

    cleaning up

--- myclient.py
+++ myclient.py
@@ -1,25 +1,7 @@
-# import asyncio
-# from fastmcp import Client
-#
-# client = Client("myserver.py")
-#
-# async def call_tool(file: str, function: str)-> None:
-#     async with client:
-#         result = await client.call_tool("optimize_code", {"file": f"{file}", "function":f"{function}"})
-#         print(result)
-#
-# asyncio.run(call_tool("Ford","Mustang"))
-
-
 import anthropic
 from rich import print
-
-# Your server URL (replace with your actual URL)
 url = 'https://0de03d07f8b9.ngrok-free.app'
-
 client = anthropic.Anthropic()
-
-#file and fn names are hard coded for now
 response = client.beta.messages.create(
     model="claude-sonnet-4-20250514",
     max_tokens=1000,
@@ -28,39 +10,11 @@ response = client.beta.messages.create(
         {
             "type": "url",
             "url": f"{url}/mcp/",
-            "name": "HelpfulAssistant",
+            "name": "Code Optimization Assistant",
         }
     ],
     extra_headers={
         "anthropic-beta": "mcp-client-2025-04-04"
     }
 )
-
 print(response.content)
-
-
-# import anthropic
-# from rich import print
-#
-# # Your server URL (replace with your actual URL)
-# url = 'https://0de03d07f8b9.ngrok-free.app'
-#
-# client = anthropic.Anthropic()
-#
-# response = client.beta.messages.create(
-#     model="claude-sonnet-4-20250514",
-#     max_tokens=1000,
-#     messages=[{"role": "user", "content": "Roll a few dice!"}],
-#     mcp_servers=[
-#         {
-#             "type": "url",
-#             "url": f"{url}/mcp/",
-#             "name": "Dice Roller",
-#         }
-#     ],
-#     extra_headers={
-#         "anthropic-beta": "mcp-client-2025-04-04"
-#     }
-# )
-#
-# print(response.content)
