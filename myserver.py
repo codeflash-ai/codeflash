e8b9202ffe6188726693d78d377160d375e293c7
Aseem Saxena <aseem.bits@gmail.com>
2025-08-20 12:01:21 -0700

    stdio server

--- myserver.py
+++ myserver.py
@@ -1,8 +1,4 @@
-import pathlib
-
 from fastmcp import FastMCP
-
-from tests.scripts.end_to_end_test_utilities import CoverageExpectation, TestConfig, run_codeflash_command
 
 mcp = FastMCP(
     name="codeflash",
@@ -14,23 +10,30 @@ mcp = FastMCP(
 
 
 @mcp.tool
-def optimize_code(file: str, function: str) -> bool:
-    config = TestConfig(
-        file_path=pathlib.Path(file),
-        function_name=function,
-        test_framework="pytest",
-        min_improvement_x=1.0,
-        coverage_expectations=[
-            CoverageExpectation(
-                function_name=function, expected_coverage=100.0, expected_lines=[2, 3, 4, 5, 6, 7, 8, 9, 10]
-            )
-        ],
-    )
-    cwd = (pathlib.Path(__file__).parent / "code_to_optimize").resolve()  # TODO remove it
-    return run_codeflash_command(
-        cwd, config, 100, ['print("codeflash stdout: Sorting list")', 'print(f"result: {arr}")']
-    )
+def optimize_code(file: str, function: str) -> dict[str, str]:
+    # config = TestConfig(
+    #     file_path=pathlib.Path(file),
+    #     function_name=function,
+    #     test_framework="pytest",
+    #     min_improvement_x=1.0,
+    #     coverage_expectations=[
+    #         CoverageExpectation(
+    #             function_name=function, expected_coverage=100.0, expected_lines=[2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #         )
+    #     ],
+    # )
+    # cwd = pathlib.Path("/Users/aseemsaxena/Downloads/codeflash_dev/codeflash/code_to_optimize")  # TODO remove it
+    print(file, function)
+    return {
+        "code": """def sorter(arr):
+    print("codeflash stdout: Sorting list")
+    arr.sort()
+    print(f"result: {arr}")
+    return arr
+""",
+        "explanation": "A faster version of the code is using python's in-built timsort function",
+    }
 
 
 if __name__ == "__main__":
-    mcp.run(transport="http", port=8000)
+    mcp.run(transport="stdio")
