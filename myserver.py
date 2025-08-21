a4b12b45865eac1f95d82daf589a512b93ce326f
aseembits93 <aseem.bits@gmail.com>
2025-08-21 13:35:07 -0700

    works with absolute paths inside codeflash

--- myserver.py
+++ myserver.py
@@ -1,4 +1,7 @@
 from fastmcp import FastMCP
+from pathlib import Path
+
+from tests.scripts.end_to_end_test_utilities import TestConfig, run_codeflash_command
 
 mcp = FastMCP(
     name="codeflash",
@@ -11,21 +14,20 @@ mcp = FastMCP(
 
 @mcp.tool
 def optimize_code(file: str, function: str) -> str:
-    # config = TestConfig(
-    #     file_path=pathlib.Path(file),
-    #     function_name=function,
-    #     test_framework="pytest",
-    #     min_improvement_x=1.0,
-    #     coverage_expectations=[
-    #         CoverageExpectation(
-    #             function_name=function, expected_coverage=100.0, expected_lines=[2, 3, 4, 5, 6, 7, 8, 9, 10]
-    #         )
-    #     ],
-    # )
-    # cwd = pathlib.Path("/Users/aseemsaxena/Downloads/codeflash_dev/codeflash/code_to_optimize")  # TODO remove it
-    print(file, function)
-    return "the function is already optimal"
+    # TODO ask for pr or no pr if successful
+    config = TestConfig(
+            file_path=Path(f"{file}"),
+            function_name=f"{function}",
+            test_framework="pytest",
+        )
+    cwd = Path(file).resolve().parent
+    status = run_codeflash_command(cwd, config, expected_improvement_pct=5)
+    if status:
+        "Optimization Successful, file has been edited"
+    else:
+        return "Codeflash run did not meet expected requirements for testing, reverting file changes."
 
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
+    # Optimize my codebase, the file is "/Users/codeflash/Downloads/codeflash-dev/codeflash/code_to_optimize/bubble_sort.py" and the function is "sorter"
