90ebac354c66dda842b93e0c72b3e7f844ca62f8
ali <mohammed18200118@gmail.com>
2025-08-26 12:50:00 +0300

    revert comment

--- codeflash/lsp/beta.py
+++ codeflash/lsp/beta.py
@@ -145,8 +145,7 @@ def validate_project(server: CodeflashLanguageServer, _params: FunctionOptimizat
         server.show_message_log("pyproject.toml is not valid", "Error")
         return {
             "status": "error",
-            # keep the error message the same, the extension is matching "pyproject.toml" in the error message to show the codeflash init instructions
-            "message": "pyproject.toml is not valid",
+            "message": "pyproject.toml is not valid",  # keep the error message the same, the extension is matching "pyproject.toml" in the error message to show the codeflash init instructions
         }
 
     args = process_args(server)
