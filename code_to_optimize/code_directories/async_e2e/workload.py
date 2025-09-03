d8a6ab27dc3e4863cad512099a400c1f8be2bc47
Kevin Turcios <turcioskevinr@gmail.com>
2025-09-03 01:24:04 -0500

    temp

--- code_to_optimize/code_directories/async_e2e/workload.py
+++ code_to_optimize/code_directories/async_e2e/workload.py
@@ -1,12 +1,5 @@
 import asyncio
+
 async def fake_api_call(delay, data):
     await asyncio.sleep(0.0001)
     return f"Processed: {data}"
-
-
-async def some_api_call(urls):
-    results = []
-    for url in urls:
-        res = await fake_api_call(1, url)
-        results.append(res)
-    return results
