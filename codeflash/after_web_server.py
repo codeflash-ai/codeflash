558e975134452bfedbad54f71aff78cb2130f835
Saurabh Misra <misra.saurabh1@gmail.com>
2025-09-04 17:28:57 -0700

    web servers
    Signed-off-by: Saurabh Misra <misra.saurabh1@gmail.com>

--- codeflash/after_web_server.py
+++ codeflash/after_web_server.py
@@ -6,5 +6,6 @@ async def get_endpoint(session, url):
 async def some_api_call(urls):
     async with aiohttp.ClientSession() as session:
         tasks = [get_endpoint(session, url) for url in urls]
+        # Run requests concurrently
         results = await asyncio.gather(*tasks)
         return results
