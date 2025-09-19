cf876b62dc62e9d01243f6360d4b34c8fbf72016
ali <mohammed18200118@gmail.com>
2025-09-19 15:47:49 +0300

    manually install the extension from openvsx

--- codeflash/cli_cmds/cmd_init.py
+++ codeflash/cli_cmds/cmd_init.py
@@ -24,7 +24,7 @@ from rich.text import Text
 from codeflash.api.cfapi import is_github_app_installed_on_repo
 from codeflash.cli_cmds.cli_common import apologize_and_exit
 from codeflash.cli_cmds.console import console, logger
-from codeflash.cli_cmds.vscode import install_vscode_extension
+from codeflash.cli_cmds.extension import install_vscode_extension
 from codeflash.code_utils.compat import LF
 from codeflash.code_utils.config_parser import parse_config_file
 from codeflash.code_utils.env_utils import check_formatter_installed, get_codeflash_api_key
