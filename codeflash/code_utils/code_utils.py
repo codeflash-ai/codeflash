b2bb31ebfa378fd8eaa6040ea6ed610e3fd8cdc7
ali <mohammed18200118@gmail.com>
2025-10-21 14:53:37 +0300

    ignore ruff lint warning

--- codeflash/code_utils/code_utils.py
+++ codeflash/code_utils/code_utils.py
@@ -366,7 +366,7 @@ def extract_unique_errors(pytest_output: str) -> set[str]:
     pattern = r"^E\s+(.*)$"
 
     for error_message in re.findall(pattern, pytest_output, re.MULTILINE):
-        error_message = error_message.strip()
+        error_message = error_message.strip()  # noqa: PLW2901
         if error_message:
             unique_errors.add(error_message)
 
