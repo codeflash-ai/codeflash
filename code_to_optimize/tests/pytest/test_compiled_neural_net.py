76a4fbef7beffe561ae132cb2fac76cfed571e38
Aseem Saxena <aseem.bits@gmail.com>
2026-01-23 07:55:58 +0000

    workload on gpu

--- code_to_optimize/tests/pytest/test_compiled_neural_net.py
+++ code_to_optimize/tests/pytest/test_compiled_neural_net.py
@@ -1,11 +1,13 @@
 import torch
 from code_to_optimize.unoptimized_neural_net import UnoptimizedNeuralNet
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 torch.manual_seed(42)
-model = UnoptimizedNeuralNet(input_size=64, hidden_size=8, num_classes=5)
+model = UnoptimizedNeuralNet(input_size=64, hidden_size=8, num_classes=5).to(device)
 model = torch.compile(model)
 batch_size = 4
-input_tensor = torch.randn(batch_size, 64)
+input_tensor = torch.randn(batch_size, 64).to(device)
 for _ in range(3):
     model(input_tensor)
 
@@ -16,5 +18,5 @@ def test_compiled_neural_net():
         [0.9999979734420776, 1.7223709392055753e-06, 2.2974481785246303e-25, 6.0273253055243e-10, 3.6319553942121274e-07],
         [1.7603208387478864e-12, 1.0, 9.150994628726039e-09, 1.467387301078149e-12, 2.5555071392346917e-09],
         [9.112921543419361e-05, 0.005489135626703501, 0.6995948553085327, 0.21087759733200073, 0.08394725620746613]
-    ])
+    ]).to(device)
     assert torch.allclose(output, expected_output)
