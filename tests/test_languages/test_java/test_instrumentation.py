8ca2d646a8501ff0fbe67401a49691da9b756f74
Kevin Turcios <turcioskevinr@gmail.com>
2026-03-02 10:03:00 -0500

    fix: rename min/max_outer_loops to pytest_min/max_loops in tests

--- tests/test_languages/test_java/test_instrumentation.py
+++ tests/test_languages/test_java/test_instrumentation.py
@@ -2189,8 +2189,8 @@ public class CalculatorTest {
             test_env=test_env,
             test_files=func_optimizer.test_files,
             optimization_iteration=0,
-            min_outer_loops=1,
-            max_outer_loops=1,
+            pytest_min_loops=1,
+            pytest_max_loops=1,
             testing_time=0.1,
         )
 
@@ -2340,8 +2340,8 @@ public class MathUtilsTest__perfonlyinstrumented {
             test_env=test_env,
             test_files=func_optimizer.test_files,
             optimization_iteration=0,
-            min_outer_loops=1,
-            max_outer_loops=1,  # Only 1 outer loop (Maven invocation)
+            pytest_min_loops=1,
+            pytest_max_loops=1,  # Only 1 outer loop (Maven invocation)
             testing_time=1.0,
         )
 
@@ -2464,8 +2464,8 @@ public class StringUtilsTest {
             test_env=test_env,
             test_files=func_optimizer.test_files,
             optimization_iteration=0,
-            min_outer_loops=1,
-            max_outer_loops=1,
+            pytest_min_loops=1,
+            pytest_max_loops=1,
             testing_time=0.1,
         )
 
@@ -2566,8 +2566,8 @@ public class BrokenCalcTest {
             test_env=test_env,
             test_files=func_optimizer.test_files,
             optimization_iteration=0,
-            min_outer_loops=1,
-            max_outer_loops=1,
+            pytest_min_loops=1,
+            pytest_max_loops=1,
             testing_time=0.1,
         )
 
@@ -2751,8 +2751,8 @@ public class CounterTest__perfinstrumented {
             test_env=test_env,
             test_files=func_optimizer.test_files,
             optimization_iteration=0,
-            min_outer_loops=1,
-            max_outer_loops=1,
+            pytest_min_loops=1,
+            pytest_max_loops=1,
             testing_time=0.1,
         )
 
