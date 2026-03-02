a922a29a99cf1a9f174e681e669b6ece40ca6728
aseembits93 <aseem.bits@gmail.com>
2026-03-03 05:15:26 +0530

    persistent session not needed

--- codeflash/api/cfapi.py
+++ codeflash/api/cfapi.py
@@ -27,8 +27,6 @@ if TYPE_CHECKING:
 
 from packaging import version
 
-cfapi_session = requests.Session()
-
 
 @dataclass
 class BaseUrls:
@@ -79,9 +77,9 @@ def make_cfapi_request(
         if method.upper() == "POST":
             json_payload = json.dumps(payload, indent=None, default=pydantic_encoder)
             cfapi_headers["Content-Type"] = "application/json"
-            response = cfapi_session.post(url, data=json_payload, headers=cfapi_headers, timeout=60)
+            response = requests.post(url, data=json_payload, headers=cfapi_headers, timeout=60)
         else:
-            response = cfapi_session.get(url, headers=cfapi_headers, params=params, timeout=60)
+            response = requests.get(url, headers=cfapi_headers, params=params, timeout=60)
         response.raise_for_status()
         return response
     except requests.exceptions.HTTPError:
